501f3cc1d7e490a542f6a6314165668640171b4b
Danakane <li.3b@hotmail.fr>
2016-05-14 02:07:04 +0200

    Beans files

---
+++ src/beans/Answer.java
@@ -0,0 +1,71 @@
+package beans;
+
+public class Answer 
+{
+	private Long		m_lId;
+	private String 		m_sText;
+	private Long 		m_lOrder;
+	private boolean 	m_bActive;
+	private boolean 	m_bValide;
+	private Question 	m_question;
+	
+	public Long getId()
+	{
+		return m_lId;
+	}
+	public void setId(Long lId)
+	{
+		m_lId = lId;
+	}
+	
+	public String getText()
+	{
+		return m_sText;
+	}
+	public void setText(String sText)
+	{
+		m_sText = sText;
+	}
+	
+	public Long getOrder()
+	{
+		return m_lOrder;
+	}
+	public void setOrder(Long lOrder)
+	{
+		m_lOrder = lOrder;
+	}
+	
+	public String getActive()
+	{
+		if(m_bActive)
+			return "active";
+		else
+			return "inactive";
+	}
+	public void setActive(boolean bActive)
+	{
+		m_bActive = bActive;
+	}
+	
+	public String getValide()
+	{
+		if(m_bValide)
+			return "valide";
+		else
+			return "invalide";
+	}
+	public void setValide(boolean bValide)
+	{
+		m_bValide = bValide;
+	}
+	
+	public Question getQuestion()
+	{
+		return m_question;
+	}
+	public void setQuestion(Question question)
+	{
+		m_question = question;
+	}
+}
